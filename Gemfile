62ba2f515c9d96c84b2e20e020708f31e0b74ac8
ale88andr <ale88andr@gmail.com>
2013-05-11 23:15:53 +0300

    Initial release

---
+++ Gemfile
@@ -0,0 +1,26 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.13'
+
+# database
+gem 'sqlite3'
+
+# view
+gem 'haml-rails'
+
+# media
+gem 'carrierwave'
+
+# auth
+
+# other
+gem 'kaminari'
+
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyDraft::Application.config.secret_token = 'bb062e82903b775119935767b615cf7472e449fda32300ac025398a39e7df2e10150ebf53c17d39f3729a36b1a10448522caf2b9dd367246e5e70c8ebd566851'
